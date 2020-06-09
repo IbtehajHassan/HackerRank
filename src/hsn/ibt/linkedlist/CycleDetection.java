1e83f7e6ebbb60e12a3d7219954a8bdef4fcb5ee
Ibtehaj Hassan <ibtehajhassan91@gmail.com>
2020-06-09 15:22:05 -0400

    Solved https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
    Signed-off-by: Ibtehaj Hassan <ibtehajhassan91@gmail.com>

---
+++ src/hsn/ibt/linkedlist/CycleDetection.java
@@ -0,0 +1,82 @@
+package hsn.ibt.linkedlist;
+
+import java.util.Arrays;
+
+public class CycleDetection extends LinkedListPrimitiveOperations {
+
+
+    static boolean hasCycle(SinglyLinkedListNode head) {
+
+        SinglyLinkedListNode prev = head;
+        head = head.next;
+
+        int count = 0;
+        while (head != null && prev != head) {
+            if (++count % 2 == 0) {
+                prev = prev.next;
+            }
+            head = head.next;
+        }
+        if (prev == head) {
+            return true;
+        }
+        return false;
+
+    }
+
+    static SinglyLinkedListNode removeDuplicatesRecursive(SinglyLinkedListNode head) {
+
+        if (head.next == null) {
+            return head;
+        }
+        SinglyLinkedListNode returnedHead = removeDuplicatesRecursive(head.next);
+        if (head.data != returnedHead.data) {
+            head.next = returnedHead;
+            return head;
+        } else {
+            return returnedHead;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String ss =
+                "2\n" +
+                        "1\n" +
+                        "1";
+
+        String s = "9\n" +
+                "8\n" +
+                "7\n" +
+                "6\n" +
+                "5\n" +
+                "4\n" +
+                "3\n" +
+                "2\n" +
+                "1";
+
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        for (int s1 : Arrays.stream(s.split("\n")).mapToInt(Integer::parseInt).toArray()) {
+            singlyLinkedList.add(s1);
+        }
+
+        SinglyLinkedListNode cycleList = singlyLinkedList.head;
+        SinglyLinkedListNode cycleNode = singlyLinkedList.head;
+
+        int count = 0;
+        while (cycleList.next != null) {
+            if (count++ == 7) {
+                cycleNode = cycleList;
+            }
+            cycleList = cycleList.next;
+        }
+        cycleList.next = cycleNode;
+
+
+        //printLinkedList(singlyLinkedList.head);
+
+        boolean hasCycle = hasCycle(singlyLinkedList.head);
+
+        System.out.println("Has Cycle : " + hasCycle);
+    }
+}
